984c82c39dad995a3b9c3c14c128fa1700d2cbd7
Kirubel Tesfaw <167413403+Kirubel-2001@users.noreply.github.com>
2025-10-24 16:09:44 +0300

    connected mongodb

--- api/server.js
+++ api/server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
