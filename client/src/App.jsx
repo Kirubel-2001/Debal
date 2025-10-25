aceaf32bf8be6b3c0f859a560f3aa62a5ffa4852
Kirubel Tesfaw <167413403+Kirubel-2001@users.noreply.github.com>
2025-10-25 18:51:06 +0300

    Complete redux setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
-<div>
-    <h1>Hello!!!</h1>
-</div>
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
+function App() {
+    return(
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+        </BrowserRouter>
+    )
+}
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { store, persistor } from './redux/store'
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
-  </StrictMode>
+    </PersistGate>
+  </Provider>
 );
